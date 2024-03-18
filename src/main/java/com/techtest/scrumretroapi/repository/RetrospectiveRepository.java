c860663e27177dda601a3bf68e148665d01af4c6
Luke Martin <luke.martin@alliedoffsets.com>
2024-03-18 14:55:25 +0000

    Updated methods for creating and editing feedback for retrospectives

--- src/main/java/com/techtest/scrumretroapi/repository/RetrospectiveRepository.java
+++ src/main/java/com/techtest/scrumretroapi/repository/RetrospectiveRepository.java
@@ -1,10 +1,13 @@
 package com.techtest.scrumretroapi.repository;
 
 import com.techtest.scrumretroapi.entity.Retrospective;
+import com.techtest.scrumretroapi.entity.feedback.Feedback;
 import com.techtest.scrumretroapi.entity.feedback.FeedbackItem;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -23,14 +26,4 @@ public interface RetrospectiveRepository extends JpaRepository<Retrospective, Lo
     @Query("SELECT r FROM Retrospective r WHERE r.name = :name")
     Retrospective findByName(@Param("name") String name);
 
-//    void createNewRetrospectiveWithFeedback(Retrospective retrospective) throws Exception;
-
-//    void addFeedbackToRetrospective(String retroName, FeedbackItem feedbackItem);
-//
-//    void updateFeedbackItemForRetrospective(String retroName, int itemId, FeedbackItem newFeedbackItem);
-
-//    @Modifying
-//    @Transactional
-//    @Query("UPDATE Retrospective r SET r.feedback = CONCAT(r.feedback, :feedbackItem) WHERE r.name = :name")
-//    void addFeedbackToRetrospective(@Param("name") String name, @Param("feedbackItem") FeedbackItem feedbackItem);
 }
--- src/main/java/com/techtest/scrumretroapi/service/RetrospectiveService.java
+++ src/main/java/com/techtest/scrumretroapi/service/RetrospectiveService.java
@@ -5,8 +5,6 @@ import com.techtest.scrumretroapi.entity.feedback.Feedback;
 import com.techtest.scrumretroapi.entity.feedback.FeedbackItem;
 import com.techtest.scrumretroapi.repository.RetrospectiveRepository;
 import com.techtest.scrumretroapi.utils.LoggingUtil;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.apache.commons.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -48,32 +46,65 @@ public class RetrospectiveService {
             logger.error(errorMessage);
             throw new Exception(errorMessage);
         } else {
-            // Check if feedback has been included in the create request
-            //List<Feedback> feedbackList = retrospective.getFeedback();
-
             logger.info("Creating a new retrospective with name: " + nameToCheck);
             retrospectiveRepository.save(retrospective);
             logger.info("New retrospective created successfully");
-
-//            if (feedbackList.isEmpty()) {
-//                // save the retrospective as normal
-//                retrospectiveRepository.save(retrospective);
-//                logger.info("New retrospective created successfully");
-//            }
         }
     }
 
-    public void createNewFeedbackForRetrospective(String retrospectiveName, FeedbackItem newFeedbackItem) {
-        // Do this immutably, simply make a copy of the existing feedback list, update the list with the new item and simply
+    @Transactional
+    public void createNewFeedbackForRetrospective(String retrospectiveName, FeedbackItem newFeedbackItem) throws Exception {
+        // Determine if the name exists and fetch the retrospective object with feedback list
+        Retrospective retrospective = retrospectiveRepository.findByName(retrospectiveName);
 
-//        logger.info("Adding new feedback to retrospective: " + retrospectiveName);
-//        logger.debug("Adding feedback with values: " + newFeedbackItem);
-//        retrospectiveRepository.createNewFeedbackForRetrospective(retrospectiveName, newFeedbackItem);
+        if (retrospective == null) {
+            logger.warn("Cannot add feedback to Retrospective " + retrospectiveName + ", as it doesn't exist!");
+            throw new Exception("Retrospective does not exist");
+        } else {
+            logger.info("Adding new feedback to retrospective: " + retrospectiveName);
+            logger.debug("Adding feedback with values: " + newFeedbackItem);
+            List<Feedback> feedbackList = retrospective.getFeedback();
+            // generate an item id based on current list size and add 1
+            int item = feedbackList.size() + 1;
+            feedbackList.add(new Feedback(item, newFeedbackItem));
+            retrospective.setFeedback(feedbackList);
+
+            // save the new repository
+            retrospectiveRepository.save(retrospective);
+        }
     }
 
-    public void updateFeedbackForRetrospective(String retrospectiveName, int itemId, FeedbackItem feedbackItem) {
-//        logger.info("Updating feedback for retrospective: " + retrospectiveName + ", item ID: " + itemId);
-//        logger.debug("Adding feedback with values: " + feedbackItem);
-//        retrospectiveRepository.updateFeedbackForRetrospective(retrospectiveName, itemId, feedbackItem);
+    public void updateFeedbackForRetrospective(String retrospectiveName, int itemId, FeedbackItem feedbackItem) throws Exception {
+        Retrospective retrospective = retrospectiveRepository.findByName(retrospectiveName);
+
+        if (retrospective == null) {
+            logger.warn("Cannot add feedback to Retrospective " + retrospectiveName + ", as it doesn't exist!");
+            throw new Exception("Retrospective does not exist");
+        } else {
+            logger.info("Updating feedback for retrospective: " + retrospectiveName + ", item ID: " + itemId);
+            logger.debug("Adding feedback with values: " + feedbackItem);
+
+            // copy the list and extract the feedback item
+            List<Feedback> feedbackList = retrospective.getFeedback();
+            Feedback feedbackCopy = feedbackList.stream()
+                    .filter(fi -> fi.getItem() == itemId)
+                    .findFirst()
+                    .orElseThrow(() -> {
+                        logger.warn("Unable to update feedback for feedback item number: " + itemId + ". Does not exist for retrospective " + retrospectiveName);
+                        return new Exception("No feedback with item number: " + itemId + ". Please revise item number or create new feedback");
+                    });
+
+            feedbackCopy.setItemBody(feedbackItem);
+
+            List<Feedback> updatedFeedbackList = new java.util.ArrayList<>(feedbackList.stream()
+                    .filter(fi -> fi.getItem() != itemId)
+                    .toList());
+
+            updatedFeedbackList.add(feedbackCopy);
+
+            retrospective.setFeedback(updatedFeedbackList);
+            retrospectiveRepository.save(retrospective);
+            logger.info("Successfully updated feedback item " + itemId + " with new feedback for Retrospective " + retrospectiveName);
+        }
     }
 }
